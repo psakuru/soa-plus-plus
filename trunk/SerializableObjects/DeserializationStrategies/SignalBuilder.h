3853c7d379cfac04731081804e417fff02881a55
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-06-11 18:17:10 +0000

    A ri daje

---
+++ trunk/SerializableObjects/DeserializationStrategies/SignalBuilder.h
@@ -0,0 +1,14 @@
+#ifndef SIGNALBUILDER_H
+#define SIGNALBUILDER_H
+
+#include "SerializableObjectBuilder.h"
+
+class SignalBuilder : public SerializableObjectBuilder
+{
+    public:
+    	SignalBuilder();
+    	~SignalBuilder();
+        virtual SerializableObject* delegateBuilding(Type typeToBuild, uint64_t valueLength, void* value);
+};
+
+#endif // SIGNALBUILDER_H
