6f43fbeaa15337ef5b862fdf455a4cd6184eaf8a
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-06-17 16:59:26 +0000

    Yeah

--- Thread/main.cpp
+++ Thread/main.cpp
@@ -1,8 +1,19 @@
 #include "Thread.h"
+#include <iostream>
+using namespace std;
+
+class MyThread : public Thread
+	{
+	public:
+		void run()
+			{
+			cout << "Virtual awesomeness" << endl;
+			}
+	};
 
 int main()
 	{
-	Thread t;
+	MyThread t;
 	t.join();
 	return 0;
 	}
