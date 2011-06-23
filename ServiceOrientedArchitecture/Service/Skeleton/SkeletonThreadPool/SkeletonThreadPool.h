8a228726fb479872bc5948c2f88d9fb388bcc5a9
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-06-23 13:51:58 +0000

    Modifiche di etichetta

--- ServiceOrientedArchitecture/Service/Skeleton/SkeletonThreadPool/SkeletonThreadPool.h
+++ ServiceOrientedArchitecture/Service/Skeleton/SkeletonThreadPool/SkeletonThreadPool.h
@@ -2,26 +2,30 @@
 #define SKELETONTHREADPOOL_H
 
 #include <boost/thread/thread.hpp>
+#include <list>
 #include <iostream>
 using namespace std;
 
 template <typename T, int poolSize> //T è un PoolableCallableSkeletonWrapper
 class SkeletonThreadPool
 {
-private:
+protected:
     boost::thread_group pool;
+    list<boost::thread*> threadReferences;
 public:
     boost::mutex sharedMutex; //TODO orrore!!! Un membro dati pubblico!!! Rimediare!!!!
     TcpIpPassiveSocket sharedListeningSocket; //TODO orrore!!! Un membro dati pubblico!!! Rimediare!!!!
-    SkeletonThreadPool(int listeningPort, int backlog)
-        : sharedListeningSocket(listeningPort, backlog)
+    SkeletonThreadPool(string IPAddress, int listeningPort, int backlog)
+        : sharedListeningSocket(IPAddress, listeningPort, backlog)
     {
         cout << "SkeletonThreadPool<" << (typeid(T)).name() << ", " << poolSize << ">(" << listeningPort << ", " << backlog << ")" << endl;
         T callableObject;
         for(int i = 0; i < poolSize; i++)
         {
-            //cout << ">> SkeletonThreadPool: aggiungo un thread" << endl;
-            pool.add_thread(new boost::thread(callableObject, &sharedMutex, &sharedListeningSocket));
+            threadReferences.push_front(new boost::thread(callableObject, &sharedMutex, &sharedListeningSocket));
+            //Inserimento in O(1), la list non diventa owner del thread
+            pool.add_thread(threadReferences.front);
+            //Inserimento in O(1), il pool diventa owner del thread e ne farà la delete.
             //TODO il precedente statement viola l' incapsulamento. Un workaround?
             //NB: il pool prende ownership sul new thread, posso stare tranquillo per la delete
         }
