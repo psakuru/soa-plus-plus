783b233b051760a4a9bcd41aa49a04d5b03ac5ed
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-06-24 08:37:29 +0000

    I fratelli minori

---
+++ trunk/ServiceOrientedArchitecture/Service/Skeleton/RegistrableSkeleton/RegistrableSkeleton.h
@@ -0,0 +1,16 @@
+#ifndef REGISTRABLESKELETON_H
+#define REGISTRABLESKELETON_H
+
+#include "../Skeleton.h"
+#include "../../../../ObjectInterfaces/RegistrableObject/RegistrableObject.h"
+
+class RegistrableSkeleton : public RegistrableObject, virtual public Skeleton
+{
+    public:
+    RegistrableSkeleton();
+    RegistrableSkeleton(string serviceIDToSet);
+    RegistrableSkeleton(string serviceIDToSet, TcpIpPassiveSocket* listeningSocketToShare);
+    string getRegistrationInfo();
+};
+
+#endif // REGISTRABLESKELETON_H
