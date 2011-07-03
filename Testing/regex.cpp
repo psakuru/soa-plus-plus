e725457efa8c9563b974aa2921cf333e0e77d1f0
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-03 11:59:55 +0000

    Prova scriptino

--- Testing/regex.cpp
+++ Testing/regex.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-string IPAddress = "227.0.0.1:49151";
+ string IPAddress = "227.0.0.1:49151";
 RegularExpressionChecker* checker = SingletonObject<RegularExpressionChecker>::getInstance();
 checker->setRegularExpression("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b\\:(4915[0-1]|491[0-4]\\d|490\\d\\d|4[0-8]\\d{3}|[1-3]\\d{4}|[2-9]\\d{3}|1[1-9]\\d{2}|10[3-9]\\d|102[4-9])");
 cout << checker->checkForMatch(IPAddress);
