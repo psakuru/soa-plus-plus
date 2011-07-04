9b75f12d09bb51761f533110cfadfe37afea1733
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-04 12:58:54 +0000

    la soa

--- trunk/SerializableObjects/TypeConstants.h
+++ trunk/SerializableObjects/TypeConstants.h
@@ -1,3 +1,26 @@
+/**
+ * @file TypeConstants.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ * Il file contiene la definizione di costanti che identificano i tipi serializzabili.
+ */
+
 #ifndef TYPECONSTANTS_H_INCLUDED
 #define TYPECONSTANTS_H_INCLUDED
 
