a94c42074c7500b0f658665c169f1cbc5ffca624
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-04 18:57:24 +0000

    Eliminati alcuni sorgenti di prova

--- trunk/ObjectInterfaces/SingletonObject/provaSingleton.cpp
+++
@@ -1,9 +0,0 @@
-#include "SingletonObject.h"
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "SingletonObject<int>::getInstance(): " << *(SingletonObject<double>::getInstance()) << endl;
-	return 0;
-}
