3444c6f72c6e1d2ea3688586a2f0a6bf18ca9596
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-04 07:47:28 +0000

    DoXyGen 3.1

--- ObjectInterfaces/RegistrableObject/RegistrableObject.h
+++ ObjectInterfaces/RegistrableObject/RegistrableObject.h
@@ -4,9 +4,41 @@
 #include <string>
 using namespace std;
 
+/**
+ * @file RegistrableObject.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @section DESCRIPTION
+ *
+ * L' interfaccia RegistrableObject fornisce, alle classi che la implementano, il metodo "getRegistrationInfo()".
+ */
+ 
 class RegistrableObject
 {
 public:
+    /**
+     * Constructor that sets the time to a given value.
+     * 
+     * @param timemillis Number of milliseconds 
+     *        passed since Jan 1, 1970.
+     *
+     * @return
+     */
     virtual string getRegistrationInfo() = 0;
 };
 
