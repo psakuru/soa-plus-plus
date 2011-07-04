fe59370cc63a88e1a20714eefd78001276051c8b
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-04 13:34:22 +0000

    serializable object - attenzione: possibile modificaaaaa

--- SerializableObjects/SerializableObject.h
+++ SerializableObjects/SerializableObject.h
@@ -74,7 +74,8 @@ public:
 	 *
 	 * @return Lunghezza del campo length.
      */
-    virtual uint64_t serialize(void** destinationBuffer) = 0;
+    virtual uint64_t serialize(void** destinationBuffer) = 0; //TODO fare tutta sta roba (x cosimo: serializzazione del tipo e della lunghezza) 
+															  // quì invece che nelle derivate, tanto è sempre uguale
     virtual void deserialize(uint64_t length, void* bufferToUse) = 0;
     virtual void setValue(void* valueToSet) = 0;
     virtual void* getValue() = 0;
