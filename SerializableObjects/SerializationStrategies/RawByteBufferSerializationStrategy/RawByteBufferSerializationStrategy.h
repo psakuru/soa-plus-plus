03df34a812081dc11b03a5190aca32d79edfbe25
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-05 08:17:07 +0000

    string + attenzione al size_t + bytearray

--- SerializableObjects/SerializationStrategies/RawByteBufferSerializationStrategy/RawByteBufferSerializationStrategy.h
+++ SerializableObjects/SerializationStrategies/RawByteBufferSerializationStrategy/RawByteBufferSerializationStrategy.h
@@ -1,3 +1,24 @@
+/**
+ * @file RawByteBufferSerializationStrategy.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef RAWBYTEBUFFER_SERIALIZATION_STRATEGY_H
 #define RAWBYTEBUFFER_SERIALIZATION_STRATEGY_H
 
@@ -6,10 +27,18 @@
 #include "../../TypeConstants.h"
 #include "../../GenericSerializableReferenceLayer/GenericSerializableReferenceLayer.h"
 
+/**
+ * @class RawByteBufferSerializationStrategy
+ *
+ * @brief Serializzazione di un' array di byte.
+ *
+ * La classe specializza il layer per trattare le array di byte.
+ *
+ */
+
 class RawByteBufferSerializationStrategy : public GenericSerializableReferenceLayer<ByteArray>
 {
 public:
-    RawByteBufferSerializationStrategy();
     RawByteBufferSerializationStrategy(ByteArray& userReferenceToSet);
     RawByteBufferSerializationStrategy(ByteArray* userReferenceToSet, bool shared);
     Type getType() const;
