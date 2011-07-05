de67e2f29641071b5a3fb2a79b284a6b51f4a23c
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-05 09:48:31 +0000

    deser

--- SerializableObjects/DeserializationStrategies/SerializableObjectBuilder.h
+++ SerializableObjects/DeserializationStrategies/SerializableObjectBuilder.h
@@ -1,3 +1,24 @@
+/**
+ * @file SerializableObjectBuilder.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef SERIALIZABLEOBJECTBUILDER_H
 #define SERIALIZABLEOBJECTBUILDER_H
 
@@ -5,6 +26,15 @@
 #include "../../ObjectInterfaces/SerializableObject/SerializableObject.h"
 #include "../TypeConstants.h"
 using namespace std;
+
+/**
+ * @class SerializableObjectBuilder
+ *
+ * @brief Classe base del sistema di building.
+ *
+ * Classe base del sistema di building per la costruzione di SerializableObject a partire dalla loro rappresentazione serializzata. 
+ *
+ */
 
 class SerializableObjectBuilder
 {
