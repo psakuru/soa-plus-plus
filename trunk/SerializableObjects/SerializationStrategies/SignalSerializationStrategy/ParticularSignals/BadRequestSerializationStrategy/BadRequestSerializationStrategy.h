01fea061e470400a563ba72ab40ac6454c51c8a4
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-05 09:41:01 +0000

    utilities come il byte array sta al cane

--- trunk/SerializableObjects/SerializationStrategies/SignalSerializationStrategy/ParticularSignals/BadRequestSerializationStrategy/BadRequestSerializationStrategy.h
+++ trunk/SerializableObjects/SerializationStrategies/SignalSerializationStrategy/ParticularSignals/BadRequestSerializationStrategy/BadRequestSerializationStrategy.h
@@ -1,8 +1,38 @@
+/**
+ * @file BadRequestSerializationStrategy.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef BADREQUEST_SERIALIZATION_STRATEGY_H
 #define BADREQUEST_SERIALIZATION_STRATEGY_H
 
 #include "../../SignalSerializationStrategy.h"
 #include "../../Utilities/BadRequestSignal/BadRequestSignal.h"
+
+/**
+ * @class BadRequestSerializationStrategy
+ *
+ * @brief Esempio di Signal.
+ *
+ * La classe specializza il segnale di "bad request", un esempio di come trattare i segnali.
+ *
+ */
 
 class BadRequestSerializationStrategy : public SignalSerializationStrategy
 {
