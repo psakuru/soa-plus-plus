ee5a050b12eba310f3ad9b604881973bc2afb8ed
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-06 14:45:15 +0000

    ctrl+s

--- ServiceOrientedArchitecture/Service/Skeleton/RegistrablePoolableCyclicCallableSkeleton/RegistrablePoolableCyclicCallableSkeleton.h
+++ ServiceOrientedArchitecture/Service/Skeleton/RegistrablePoolableCyclicCallableSkeleton/RegistrablePoolableCyclicCallableSkeleton.h
@@ -1,3 +1,24 @@
+/**
+ * @file RegistrablePoolableCyclicCallableSkeleton.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef POOLABLECYCLICCALLABLESKELETON_H
 #define POOLABLECYCLICCALLABLESKELETON_H
 
@@ -7,15 +28,36 @@
 #include "../../../../TcpIpSocket/TcpIpPassiveSocket/TcpIpPassiveSocket.h"
 #include <boost/thread/mutex.hpp>
 
+/**
+ * @class RegistrablePoolableCyclicCallableSkeleton
+ *
+ * @brief Aggiunge ulteriori funzionalità all' interfaccia Skeleton.
+ *
+ * Aggiunge allo Skeleton la possibilità di essere invocato, di registrarsi e di essere inserito in un pool di skeleton. 
+ *
+ */
+
 class RegistrablePoolableCyclicCallableSkeleton : public RegistrableSkeleton, public CallableService<Skeleton>
 {
 private:
+	/**
+	 * Mutex condiviso fra gli skeleton appartenenti al pool.
+	 */ 
     boost::mutex* sharedMutex;
 public:
     RegistrablePoolableCyclicCallableSkeleton(string serviceIDToSet); // Il ParticularPoolableCyclicCallableSkeleton conosce nel suo
     //costruttore di default il serviceIDToSet;
     virtual ~RegistrablePoolableCyclicCallableSkeleton();
+	/**
+	 * Assegna a sharedMutex il parametro mutexToShare.
+	 *
+	 * @param mutexToShare
+	 */ 
     void shareMutex(boost::mutex* mutexToShare);
+	/**
+	 * Operatore di chiamata. 
+	 * Accetta una connessione sul socket ed esegue protocol();
+	 */ 
     virtual void operator()();
 };
 
