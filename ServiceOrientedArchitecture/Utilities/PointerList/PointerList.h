156759a27b33f0daf6b9150f2be5947497ab26b4
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-06 10:09:29 +0000

    MuDa

--- ServiceOrientedArchitecture/Utilities/PointerList/PointerList.h
+++ ServiceOrientedArchitecture/Utilities/PointerList/PointerList.h
@@ -1,9 +1,38 @@
+/**
+ * @file PointerList.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef POINTERLIST_H
 #define POINTERLIST_H
 
 #include <list>
 using namespace std;
 
+/**
+ * @class PointerList
+ *
+ * @brief Generica lista di puntatori.
+ *
+ * Gestisce una generica lista di puntatori ad oggetti allocati in memoria dinamica.
+ *
+ */
 template <typename T>
 class PointerList : public list<T>
 {
@@ -16,6 +45,9 @@ public:
             delete (*i);
         }
     }
+	/**
+	 * Elimina gli oggetti puntati.
+	 */
     virtual void clear()
     {
         typename list<T>::iterator i = this->begin();
@@ -26,6 +58,9 @@ public:
             this->pop_front(); // Pop di un puntatore, non viene eliminato l' oggetto puntato.
         }
     }
+	/**
+	 * Cancella i puntatori ma non elimina gli oggetti puntati.
+	 */
 	virtual void popPointers()
 	{
 		int iterationsToEmptyList = (int)(this->size());
