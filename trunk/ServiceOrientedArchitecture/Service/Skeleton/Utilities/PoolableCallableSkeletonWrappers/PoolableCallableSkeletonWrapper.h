9b21940ddbb92dd1d1701a3d41b05e7c3bd14d8f
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-06 14:07:44 +0000

    Rename

---
+++ trunk/ServiceOrientedArchitecture/Service/Skeleton/Utilities/PoolableCallableSkeletonWrappers/PoolableCallableSkeletonWrapper.h
@@ -0,0 +1,56 @@
+/**
+ * @file PoolableCallableSkeletonWrapper.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef POOLABLECALLABLESKELETONWRAPPER_H
+#define POOLABLECALLABLESKELETONWRAPPER_H
+
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
+
+/**
+ * @class Skeleton
+ *
+ * @brief Scheletro.
+ *
+ * Uno Skeleton contiene le funzionalità di base che un servente deve possedere.
+ *
+ */
+
+template <typename T>
+class PoolableCallableSkeletonWrapper
+{
+public:
+    void operator()(boost::mutex* sharedMutex, TcpIpPassiveSocket* sharedListeningSocket)
+    {
+
+
+        cout << "TID:" << boost::this_thread::get_id() << " PoolableCallableSkeletonWrapper<" << (typeid(T)).name() << ">(sharedMutex: " << (void*)sharedMutex << ", sharedListeningSocket: " << (void*)sharedListeningSocket << ")" << endl;
+        T poolableCallableSkeleton; //Precondizione: il costruttore di default di T deve soddisfare tutti gli antenati
+        poolableCallableSkeleton.shareMutex(sharedMutex); //Può fallire solo a tempo di compilazione!
+        poolableCallableSkeleton.shareListeningSocket(sharedListeningSocket); //Può fallire solo a tempo di compilazione!
+        poolableCallableSkeleton(); //Può fallire solo a tempo di compilazione!*/
+        //Se i precedenti tre statement non falliscono, allora o T è effettivamente figlio di Skeleton
+        //oppure funziona come uno Skeleton, ma...
+        //TODO polimorphic test!
+    }
+};
+
+#endif // POOLABLECALLABLESKELETONWRAPPER_H
---
+++ trunk/ServiceOrientedArchitecture/Service/Skeleton/Utilities/PoolableCallableSkeletonWrappers/RegistrablePoolableCallableSkeletonWrapper/RegistrablePoolableCallableSkeletonWrapper.h
@@ -0,0 +1,19 @@
+#ifndef REGISTRABLEPOOLABLECALLABLESKELETONWRAPPER_H
+#define REGISTRABLEPOOLABLECALLABLESKELETONWRAPPER_H
+
+#include "../PoolableCallableSkeletonWrapper.h"
+#include "../../../../../ObjectInterfaces/RegistrableObject/RegistrableObject.h"
+
+template <typename T>
+class RegistrablePoolableCallableSkeletonWrapper : public RegistrableObject, public PoolableCallableSkeletonWrapper<T>
+{
+public:
+    string getRegistrationInfo()
+    {
+        cout << "Fino al RegistrablePoolableCallableSkeletonWrapper ci arrivo" << endl;
+        T dummyT;
+        return dummyT.getRegistrationInfo();
+    }
+};
+
+#endif // REGISTRABLEPOOLABLECALLABLESKELETONWRAPPER_H
