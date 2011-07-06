74fe5cab991328ca3a5c777b90b439f213410739
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-06 10:21:42 +0000

    Stream Stub

--- ServiceOrientedArchitecture/Service/Stub/StreamStub/StreamStub.cpp
+++ ServiceOrientedArchitecture/Service/Stub/StreamStub/StreamStub.cpp
@@ -3,10 +3,7 @@
 #include "../../../../SerializableObjects/SerializationStrategies/RealSerializationStrategy/RealSerializationStrategy.h"
 #include "../../../../SerializableObjects/SerializationStrategies/StringSerializationStrategy/StringSerializationStrategy.h"
 #include "../../../../SerializableObjects/SerializationStrategies/RawByteBufferSerializationStrategy/RawByteBufferSerializationStrategy.h"
-#include <iostream>
-using namespace std;
 
-// NB: codice replicatissimo!
 StreamStub::StreamStub() {}
 
 StreamStub::StreamStub(string serviceIDToSet, string serviceRegistryAddressToSet)
@@ -16,72 +13,50 @@ StreamStub::~StreamStub() {}
 
 void StreamStub::operator<<(int& value)
 {
-    /*SerializableObject* objectToPushIntoList = new Integer(value);
-    outputParameters.push_back(objectToPushIntoList);*/
     addParameter(new Integer(value), OUT);
 }
 
 void StreamStub::operator<<(double& value)
 {
-    /*SerializableObject* objectToPushIntoList = new Real(value);
-    outputParameters.push_back(objectToPushIntoList);*/
     addParameter(new Real(value), OUT);
 }
 
 void StreamStub::operator<<(string& value)
 {
-    /*SerializableObject* objectToPushIntoList = new String(value);
-    outputParameters.push_back(objectToPushIntoList);*/
     addParameter(new String(value), OUT);
 }
 
 void StreamStub::operator<<(ByteArray& value)
 {
-    /*SerializableObject* objectToPushIntoList = new RawByteBuffer(value);
-    outputParameters.push_back(objectToPushIntoList);*/
     addParameter(new RawByteBuffer(value), OUT);
 }
 
 void StreamStub::operator<<(BadRequestSignal& value)
 {
-    /*SerializableObject* objectToPushIntoList = new BadRequest(value);
-    outputParameters.push_back(objectToPushIntoList);*/
-    addParameter(new BadRequest(value), OUT);
+	addParameter(new BadRequest(value), OUT);
 }
 
 void StreamStub::operator>>(int& value)
 {
-    /*SerializableObject* objectToPushIntoList = new Integer(value);
-    inputParameters.push_back(objectToPushIntoList);*/
     addParameter(new Integer(value), OUTIN);
 }
 
 void StreamStub::operator>>(double& value)
 {
-    /*SerializableObject* objectToPushIntoList = new Real(value);
-    inputParameters.push_back(objectToPushIntoList);*/
     addParameter(new Real(value), OUTIN);
 }
 
 void StreamStub::operator>>(string& value)
 {
-    /*SerializableObject* objectToPushIntoList = new String(value);
-    inputParameters.push_back(objectToPushIntoList);*/
     addParameter(new String(value), OUTIN);
 }
+
 void StreamStub::operator>>(ByteArray& value)
 {
-    /*SerializableObject* objectToPushIntoList = new RawByteBuffer(value);
-    inputParameters.push_back(objectToPushIntoList);*/
     addParameter(new RawByteBuffer(value), OUTIN);
-
 }
 
 void StreamStub::operator>>(BadRequestSignal& value)
 {
-
-    /*SerializableObject* objectToPushIntoList = new BadRequest(value);
-    inputParameters.push_back(objectToPushIntoList);*/
-
     addParameter(new BadRequest(value), OUTIN);
 }
