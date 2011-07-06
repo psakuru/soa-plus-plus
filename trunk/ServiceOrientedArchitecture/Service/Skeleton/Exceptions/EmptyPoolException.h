80db79cfc1f32b9bc9a170a078af16c1724a727d
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-06 17:18:46 +0000

    eccezine

---
+++ trunk/ServiceOrientedArchitecture/Service/Skeleton/Exceptions/EmptyPoolException.h
@@ -0,0 +1,37 @@
+/**
+ * @file EmptyPoolException.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef EMPTYPOOLEXCEPTION_H
+#define EMPTYPOOLEXCEPTION_H
+
+#include <exception>
+
+
+class EmptyPoolException : public exception
+{
+public:
+    virtual const char* what() const throw()
+    {
+        return "Thread pool is empty so it cannot be registrated.";
+    }
+};
+
+#endif // EMPTYPOOLEXCEPTION_H
