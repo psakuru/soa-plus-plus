c140b8362175155f7fbe7342dc853b9301a567df
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-06 16:42:52 +0000

    NoNoNoNomemorizzato

--- trunk/ServiceOrientedArchitecture/Service/Skeleton/SkeletonThreadPool/RegistrableSkeletonThreadPool/RegistrableSkeletonThreadPool.h
+++ trunk/ServiceOrientedArchitecture/Service/Skeleton/SkeletonThreadPool/RegistrableSkeletonThreadPool/RegistrableSkeletonThreadPool.h
@@ -1,3 +1,24 @@
+/**
+ * @file RegistrableSkeletonThreadPool.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef REGISTRABLESKELETONTHREADPOOL_H
 #define REGISTRABLESKELETONTHREADPOOL_H
 
@@ -5,25 +26,29 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @class RegistrableSkeletonThreadPool
+ *
+ * @brief Rende lo SkeletonThreadPool registrabile.
+ *
+ * Rende lo SkeletonThreadPool registrabile utilizzando l' interfaccia offerta da RegistrableObject.
+ *
+ */
+
 template <typename T, int poolSize> class RegistrableSkeletonThreadPool
     : public RegistrableObject, public SkeletonThreadPool<T, poolSize>
 {
 public:
     RegistrableSkeletonThreadPool(string IPAddress, int listeningPort, int backlog)
-        : SkeletonThreadPool<T, poolSize>(IPAddress, listeningPort, backlog)
-    {
-    }
+        : SkeletonThreadPool<T, poolSize>(IPAddress, listeningPort, backlog) {}
     string getRegistrationInfo()
     {
-        cout << "Fino a SkeletonThreadPool ci arrivo" << endl;
-        if(!SkeletonThreadPool<T, poolSize>::threadReferences.empty())
+		if(!SkeletonThreadPool<T, poolSize>::threadReferences.empty())
         {
-            cout << "Fino a SkeletonThreadPool ci arrivo" << endl;
             T dummyT;
             string registrationInfoToReturn = dummyT.getRegistrationInfo();
             registrationInfoToReturn.append(SkeletonThreadPool<T, poolSize>::sharedListeningSocket.getAddress());
-            cout << "Ecco registrationInfoToReturn: " << registrationInfoToReturn << endl;
-            return registrationInfoToReturn;
+			return registrationInfoToReturn;
         }
         else
         {
