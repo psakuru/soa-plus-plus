5a0dd62ebe1acf1dbcb69a2558087b7270cc2576
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-07 08:06:18 +0000

    prima le maps

--- trunk/ServiceOrientedArchitecture/Register/RegisterManager.h
+++ trunk/ServiceOrientedArchitecture/Register/RegisterManager.h
@@ -1,3 +1,24 @@
+/**
+ * @file RegisterManager.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef REGISTERMANAGER_H
 #define REGISTERMANAGER_H
 
@@ -8,6 +29,13 @@
 #include "../../SerializableObjects/SerializationStrategies/StringSerializationStrategy/StringSerializationStrategy.h"
 #include <string>
 using namespace std;
+
+/**
+ * @class RegisterManager
+ *
+ * @brief PRIMA LE MAPS
+ *
+ */
 
 template <template<typename, typename> class RegisterMap> /*devi passarmi una mappa template con 2 parametri Key e Elem, ci penso io a declinarli a string*/
 class RegisterManager : public RegistrablePoolableCyclicCallableSkeleton
