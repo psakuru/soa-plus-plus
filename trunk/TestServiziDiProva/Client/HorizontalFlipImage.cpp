a71bb1a88d22409e4fcba0444e18bef9e61608c2
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-07 11:57:02 +0000

    abbiamo detto sì alla soa

--- trunk/TestServiziDiProva/Client/HorizontalFlipImage.cpp
+++ trunk/TestServiziDiProva/Client/HorizontalFlipImage.cpp
@@ -1,17 +1,31 @@
+/**
+ * @file HorizontalFlipImage.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "../ServiceOrientedArchitecture/Service/Stub/StreamStub/StreamStub.h"
 
 class HorizontalFlipImage: public StreamStub {
 public:
-	HorizontalFlipImage() :
-		StreamStub("HorizontalFlipImage", "127.0.0.1:4000") {
-
-	}
-	void operator()(ByteArray& img, ByteArray& img2) {
-
-		(*this) << img;
-
-		(*this) >> img2;
-
-		bind();
+	HorizontalFlipImage() : StreamStub("HorizontalFlipImage", "127.0.0.1:4000") {}
+	void operator()(ByteArray& img, ByteArray& img2) 
+	{ 
+		(*this) << img; (*this) >> img2; bind();
 	}
 };
