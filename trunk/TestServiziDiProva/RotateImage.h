325c1b4f01934bff65ba574d43157cf73649b682
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-07 18:56:52 +0000

    Naming conventions

--- trunk/TestServiziDiProva/RotateImage.h
+++ trunk/TestServiziDiProva/RotateImage.h
@@ -24,7 +24,7 @@
 class RotateImage: public StreamStub {
 public:
 	RotateImage() : StreamStub("RotateImage", "127.0.0.1:4000") {}
-	void operator()(int& direction, ByteArray& img, ByteArray& img2) 
+	void operator()(int direction, ByteArray img, ByteArray& img2)
 	{
 		(*this) << direction; (*this) << img; (*this) >> img2; bind();
 	}
