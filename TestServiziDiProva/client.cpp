249e43f6459e1038b307a97436076121f2790779
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-08 14:27:59 +0000

    mumble mumble

---
+++ TestServiziDiProva/client.cpp
@@ -0,0 +1,90 @@
+#include "../ServiceOrientedArchitecture/Service/Stub/StreamStub/StreamStub.h"
+#include <fstream>
+#include <exception>
+#include <errno.h>
+#include <string.h>
+#include <iostream>
+#include "GetImage.h"
+#include "StoreImage.h"
+#include "GetList.h"
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	try
+	{
+		char * memblock;
+		uint64_t size = 0;
+		ifstream file (argv[1], ios::in|ios::binary|ios::ate);
+		if (file.is_open())
+		{
+			size = file.tellg();
+			memblock = new char [size];
+			file.seekg (0, ios::beg);
+			file.read (memblock, size);
+			file.close();
+		}
+		else 
+		{
+			cout << "Unable to open file";
+		}
+		ByteArray fileBytes((void*)memblock, size);
+		delete[] memblock;
+		StoreImage p;
+		p(argv[1], fileBytes);
+		GetList z;
+		string lista;
+		z(lista);
+		char_separator<char> separator("\\");
+		tokenizer<char_separator<char>> parser(lista);
+		cout << "LISTA: " << endl;
+		BOOST_FOREACH(string entry, parser)
+		{
+			cout << entry << endl;
+		}
+		vector<string> entries(parser.begin(), parser.end());
+		int random = rand() % entries.size();
+		string nameOfFileToReceive = entries[random];
+		GetImage s;
+		ByteArray fileRicevuto;
+		s(nameOfFileToReceive, fileRicevuto);
+		ofstream outfile (nameOfFileToReceive.c_str(),ofstream::binary | ofstream::out);
+		outfile.write((char*)fileRicevuto.getPointer(),fileRicevuto.getLength());
+		outfile.close();
+		cout << "FILE SALVATO: " << nameOfFileToReceive << endl;
+		random = rand() % 2;
+		ByteArray fileDaRicevere;
+		if (random == 0)
+		{
+			HorizontalFlipImage hfi;
+			hfi(fileRicevuto,fileDaRicevere);
+			s(nameOfFileToReceive, fileDaRicevere);
+			ofstream outfile (nameOfFileToReceive.c_str(),ofstream::binary | ofstream::out);
+			outfile.write((char*)fileDaRicevere.getPointer(),fileDaRicevere.getLength());
+			outfile.close();			
+		}
+		else
+		{
+			RotateImage ri;
+			random = rand() % 361;
+			ri(random,fileRicevuto,fileDaRicevere);
+			s(nameOfFileToReceive, fileDaRicevere);
+			ofstream outfile (nameOfFileToReceive.c_str(),ofstream::binary | ofstream::out);
+			outfile.write((char*)fileDaRicevere.getPointer(),fileDaRicevere.getLength());
+			outfile.close();			
+		}
+		StoreImage si;
+		si(nameOfFileToReceive,fileDaRicevere);
+	    cout << "fine." << endl;
+    }
+    catch(const exception& e)
+	{
+    	cout << "MUMBLE MUMBLE: " << e.what() << endl;
+	}
+    catch(...)
+    {
+        cout << "ARRIVA QUALCOSA" << endl;
+    }
+    return 0;
+}
+
