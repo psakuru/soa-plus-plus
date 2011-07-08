a0956433f82d73d497fdb27b701be7cec030279e
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-08 09:28:29 +0000

    fatto3

--- trunk/TestServiziDiProva/ImageRegisterServer.h
+++ trunk/TestServiziDiProva/ImageRegisterServer.h
@@ -63,15 +63,15 @@ private:
 	void getImage() 
 	{
 		boost::shared_lock<boost::shared_mutex> readersLock(mutex);
-		if(imageList.find_if(imageList.begin(),imageList.end(),strncmp(name))!=imageList.end())
-		{
-			//TODO ECCEZIONE - il file non esiste o comunque non è stato registrato.
-		}
 		list<string> imageList = SingletonObject< list <string> >::getInstance(); // Istanza singleton.
 		SerializableObjectList::iterator i = inputParameters.begin();
 		string* entry = (string*)(*i)->getValue();
 		string name = *entry;
 		delete entry;
+		if(imageList.find_if(imageList.begin(),imageList.end(),strncmp(name))!=imageList.end())
+		{
+			//TODO ECCEZIONE - il file non esiste o comunque non è stato registrato.
+		}
 		char * memblock;
 		uint64_t size = 0;
 		ifstream file(name, ios::in | ios::binary | ios::ate);
