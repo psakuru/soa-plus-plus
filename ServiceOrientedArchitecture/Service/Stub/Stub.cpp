74a985cab317d2c44049ad23bfd1afcf4f6f70cb
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-09 08:41:03 +0000

    Minor changes

--- ServiceOrientedArchitecture/Service/Stub/Stub.cpp
+++ ServiceOrientedArchitecture/Service/Stub/Stub.cpp
@@ -77,7 +77,12 @@ void Stub::staticallyBind(string serviceProviderAddressToSet)
     try
     {
         RegularExpressionChecker* IPAndPortRegexChecker = SingletonObject<RegularExpressionChecker>::getInstance();
-        IPAndPortRegexChecker->setRegularExpression("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b\\:(4915[0-1]|491[0-4]\\d|490\\d\\d|4[0-8]\\d{3}|[1-3]\\d{4}|[2-9]\\d{3}|1[1-9]\\d{2}|10[3-9]\\d|102[4-9])");
+        IPAndPortRegexChecker->setRegularExpression
+        (
+            "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b\
+            \\:\
+            (4915[0-1]|491[0-4]\\d|490\\d\\d|4[0-8]\\d{3}|[1-3]\\d{4}|[2-9]\\d{3}|1[1-9]\\d{2}|10[3-9]\\d|102[4-9])"
+        ); //Matcha con IP:port
         if(!IPAndPortRegexChecker->checkForMatch(serviceProviderAddressToSet))
         {
             throw InvalidAddressException();
