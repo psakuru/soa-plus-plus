7e71d7834916c0bde4592d802d2a6844578b7d14
cosimosacco@gmail.com <cosimosacco@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-09 08:22:49 +0000

    Controllo delle entry passate al registry con espressioni regolary. :)

--- trunk/Testing/regex.cpp
+++ trunk/Testing/regex.cpp
@@ -1,15 +1,15 @@
-#include "../RegularExpressionChecker/RegularExpressionChecker.h"
+#include "../Utilities/RegularExpressionChecker/RegularExpressionChecker.h"
 #include "../ObjectInterfaces/SingletonObject/SingletonObject.h"
 using namespace std;
 
 int main()
 {
- string IPAddress = "227.0.0.1:49151";
+ string IPAddress = "redHat( IN: int INOUT: string INOUT: gopher)@227.0.0.1:49151";
 RegularExpressionChecker* checker = SingletonObject<RegularExpressionChecker>::getInstance();
-checker->setRegularExpression("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b\\:(4915[0-1]|491[0-4]\\d|490\\d\\d|4[0-8]\\d{3}|[1-3]\\d{4}|[2-9]\\d{3}|1[1-9]\\d{2}|10[3-9]\\d|102[4-9])");
+checker->setRegularExpression("\\w*\\((\\s(IN|INOUT)\\:\\s\\w*)*\\)\\@\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b\\:(4915[0-1]|491[0-4]\\d|490\\d\\d|4[0-8]\\d{3}|[1-3]\\d{4}|[2-9]\\d{3}|1[1-9]\\d{2}|10[3-9]\\d|102[4-9])");
 cout << checker->checkForMatch(IPAddress);
-checker->setRegularExpression("abc");
-cout << checker->checkForMatch("no");
+cout << "prova\
+a capo ";
 return 0;
 }
 
