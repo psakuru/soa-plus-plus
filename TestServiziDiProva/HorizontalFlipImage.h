582dfe5cde6a545bd3ab6b5985e975e0b36cca67
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-09 12:57:36 +0000

    rivendedo i test

--- TestServiziDiProva/HorizontalFlipImage.h
+++ TestServiziDiProva/HorizontalFlipImage.h
@@ -26,6 +26,8 @@ public:
 	HorizontalFlipImage() : StreamStub("HorizontalFlipImage", "127.0.0.1:4000") {}
 	void operator()(ByteArray img, ByteArray& img2) 
 	{ 
-		(*this) << img; (*this) >> img2; bind(); protocol();
+		(*this) << img; (*this) >> img2; 
+		bind(); 
+		protocol();
 	}
 };
