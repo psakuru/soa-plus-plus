9b2636f4cb5a59c3531e196dff57b1f9b999b8fa
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-09 14:24:40 +0000

    omggg

---
+++ trunk/SerializableObjects/SerializationStrategies/SignalSerializationStrategy/Exceptions/ImageNotFoundException/ImageNotFoundException.h
@@ -0,0 +1,43 @@
+/**
+ * @file ImageNotFoundException.h
+ * @author  Sacco Cosimo <cosimosacco@gmail.com>, Silvestri Davide <davidesil.web@gmail.com>
+ *
+ * @section LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef IMAGENOTFOUNDEXCEPTION_H
+#define IMAGENOTFOUNDEXCEPTION_H
+
+#include <exception>
+
+/**
+ * @class ImageNotFoundException
+ *
+ * @brief Immagine non presente sul server.
+ *
+ */
+
+class ImageNotFoundException : public exception
+{
+public:
+    virtual const char* what() const throw()
+    {
+        return "Attenzione. L'immagine richiesta non è presente sul server.";
+    }
+};
+
+#endif // IMAGENOTFOUNDEXCEPTION_H
