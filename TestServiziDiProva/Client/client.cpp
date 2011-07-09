28f985c05b9bad558d476b3ec5446d0faae9bf00
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-09 13:46:34 +0000

    anche client

--- TestServiziDiProva/Client/client.cpp
+++ TestServiziDiProva/Client/client.cpp
@@ -21,13 +21,13 @@ int main(int argc, char** argv)
 {
 	try
 	{
-		char * memblock;
-		uint64_t size = 0;
 		if(argc < 1)
 		{
 			cout << "Necessita del parametro [nome_file_da_inviare].jpg";
 			return 0;
 		}
+		char * memblock;
+		uint64_t size = 0;
 		ifstream file (argv[1], ios::in|ios::binary|ios::ate);
 		if (file.is_open())
 		{
@@ -43,8 +43,10 @@ int main(int argc, char** argv)
 		}
 		ByteArray fileBytes((void*)memblock, size);
 		delete[] memblock;
+		cout << "Richiedo il servizio StoreImage.";
 		StoreImage p;
 		p(argv[1], fileBytes);
+		cout << "Richiedo la lista di immagini registrate.";
 		GetList z;
 		string lista;
 		z(lista);
@@ -56,21 +58,24 @@ int main(int argc, char** argv)
 			cout << entry <<endl;
 		}
 		vector<string> entries(parser.begin(), parser.end());
+		// Scelgo a caso un'immagine da ricevere.
 		srand( time(NULL) );
 		int random = rand() % entries.size();
 		string nameOfFileToReceive = entries[random];
+		cout << "Richiedo un'immagine a caso fra quelle presenti sul server.";
 		GetImage s;
 		ByteArray fileRicevuto;
 		s(nameOfFileToReceive, fileRicevuto);
 		ofstream outfile (nameOfFileToReceive.c_str(),ofstream::binary | ofstream::out);
 		outfile.write((char*)fileRicevuto.getPointer(),fileRicevuto.getLength());
 		outfile.close();
-		cout << "FILE SALVATO: " << nameOfFileToReceive << endl;
+		cout << "Immagine ricevuta e salvata: " << nameOfFileToReceive << endl;
+		//Richiedo a caso il servizio di flip o quello di rotate sull'immagine appena ricevuta.
 		random = rand() % 2;
-		cout << "RANDOM MICA TANTO: "<< random << endl;
 		ByteArray fileDaRicevere;
 		if (random == 0)
 		{
+			cout << "Richiedo il servizio di HorizontalFlip sull'immagine: " << nameOfFileToReceive << endl;
 			HorizontalFlipImage hfi;
 			hfi(fileRicevuto,fileDaRicevere);
 			ofstream outfile (nameOfFileToReceive.c_str(),ofstream::binary | ofstream::out);
@@ -79,25 +84,24 @@ int main(int argc, char** argv)
 		}
 		else
 		{
+			cout << "Richiedo il servizio di Rotate sull'immagine: " << nameOfFileToReceive;
 			RotateImage ri;
+			// L'angolo di rotazione viene scelto a caso.
 			random = rand();
+			cout << " di un angolo di " << random << " gradi." << endl;
 			ri(random,fileRicevuto,fileDaRicevere);
 			ofstream outfile (nameOfFileToReceive.c_str(),ofstream::binary | ofstream::out);
 			outfile.write((char*)fileDaRicevere.getPointer(),fileDaRicevere.getLength());
 			outfile.close();			
 		}
+		cout << "Richiedo il servizio di StoreImage sull'immagine modificata: " << nameOfFileToReceive << endl;
 		StoreImage si;
 		si(nameOfFileToReceive,fileDaRicevere);
-	    cout << "fine." << endl;
     }
     catch(const exception& e)
 	{
-    	cout << "MUMBLE MUMBLE: " << e.what() << endl;
+    	cout e.what() << endl;
 	}
-    catch(...)
-    {
-        cout << "ARRIVA QUALCOSA" << endl;
-    }
-    return 0;
+	return 0;
 }
 
