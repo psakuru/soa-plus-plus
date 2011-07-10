978fb9cecd9daf385f7fb0ec945a7281770403fc
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-10 11:02:34 +0000

    fatto. devo ricontrollare gli include.

---
+++ Test/Servers/GetListServer.cpp
@@ -0,0 +1,37 @@
+#include "../../ObjectInterfaces/SingletonObject/SingletonObject.h"
+#include "../../ObjectInterfaces/SerializableObject/SerializableObject.h"
+#include "../../SerializableObjects/Utilities/ByteArray/ByteArray.h"
+#include "../../ServiceOrientedArchitecture/Service/Skeleton/Utilities/PoolableCallableSkeletonWrappers/RegistrablePoolableCallableSkeletonWrapper/RegistrablePoolableCallableSkeletonWrapper.h"
+#include "../../ServiceOrientedArchitecture/Service/Skeleton/SkeletonThreadPool/RegistrableSkeletonThreadPool/RegistrableSkeletonThreadPool.h"
+#include "../../SerializableObjects/SerializationStrategies/RawByteBufferSerializationStrategy/RawByteBufferSerializationStrategy.h"
+#include "../../SerializableObjects/SerializationStrategies/IntegerSerializationStrategy/IntegerSerializationStrategy.h"
+#include "../../SerializableObjects/SerializationStrategies/StringSerializationStrategy/StringSerializationStrategy.h"
+#include "../../ServiceOrientedArchitecture/Publisher/Publisher.h"
+#include "../../ObjectInterfaces/RegistrableObject/RegistrableObject.h"
+#include <string>
+#include <list>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void GetList::doService()
+{
+	// Acquisisco il lock sulla lista.
+	boost::shared_lock<boost::shared_mutex> readersLock(sharedState->sharedMutex);
+	// Inserisco la lista nei parametri di output.
+	string stringToReturn;
+	list<string>::iterator i = sharedState->imageList.begin();
+	for(; i != sharedState->imageList.end(); i++)
+	{
+		stringToReturn.append(*i).append(";");
+	}
+	outputParameters.push_back(new String(new string(stringToReturn), false));
+}
+GetList::GetList() : Skeleton("GetList"), RegistrablePoolableCyclicCallableSkeleton("GetList")
+{
+	// Stato condiviso fra i thread del pool. E' un'istanza singleton.
+	sharedState = SingletonObject<ImageRegisterSharedState>::getInstance();
+	// Aggiungo i parametri che mi aspetto di ricevere al serviceID e alla lista di input.
+	addParameter(new String, INOUT);
+}
+
---
+++ Test/Servers/HorizontalFlipImageServer.cpp
@@ -0,0 +1,60 @@
+#define cimg_use_jpeg
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <exception>
+#include "../../ObjectInterfaces/SerializableObject/SerializableObject.h"
+#include "../../SerializableObjects/Utilities/ByteArray/ByteArray.h"
+#include "../../ServiceOrientedArchitecture/Service/Skeleton/RegistrablePoolableCyclicCallableSkeleton/RegistrablePoolableCyclicCallableSkeleton.h"
+#include "../../ServiceOrientedArchitecture/Service/Skeleton/Utilities/PoolableCallableSkeletonWrappers/RegistrablePoolableCallableSkeletonWrapper/RegistrablePoolableCallableSkeletonWrapper.h"
+#include "../../ServiceOrientedArchitecture/Service/Skeleton/SkeletonThreadPool/RegistrableSkeletonThreadPool/RegistrableSkeletonThreadPool.h"
+#include "../../SerializableObjects/SerializationStrategies/RawByteBufferSerializationStrategy/RawByteBufferSerializationStrategy.h"
+#include "../../ServiceOrientedArchitecture/Publisher/Publisher.h"
+#include "../../ObjectInterfaces/RegistrableObject/RegistrableObject.h"
+#include "../CImg/CImg.h"
+using namespace std;
+using namespace cimg_library;
+
+void HorizontalFlipImage::doService()
+{
+	// Recupero i parametri di input.
+	SerializableObjectList::iterator i = inputParameters.begin();
+	SerializableObject* r = (*i);
+	ByteArray* pb = (ByteArray*)(r->getValue());
+	// Salvo l'immagine ricevuta.
+	string name;
+	stringstream threadIDToStringConverter;
+	threadIDToStringConverter << boost::this_thread::get_id();
+	threadIDToStringConverter >> name;
+	name.append(".jpg");
+	ofstream outfile (name.c_str(),ofstream::binary | ofstream::out);
+	outfile.write( (char*)( pb->getPointer() ) , pb->getLength() );
+	outfile.close();
+	delete pb;
+	// Eseguo il flip.
+	CImg<unsigned char> image;
+	image = image.load_jpeg(name.c_str());
+	image.mirror('x');
+	image.save_jpeg(name.c_str(),90U);
+	// Inserisco l'immagine modificata nei parametri di output in modo che sia inviata come risposta.
+	char * memblock;
+	uint64_t size = 0;
+	ifstream file (name.c_str(), ios::in|ios::binary|ios::ate);
+	size = (int)file.tellg();
+	memblock = (char*)malloc(size);
+	file.seekg (0, ios::beg);
+	file.read (memblock, size);
+	file.close();
+	ByteArray* fileBytes = new ByteArray((void*)memblock, size);
+	free(memblock);
+	remove(name.c_str());
+	RawByteBuffer* objectToBeSent = new RawByteBuffer(fileBytes, false);
+	outputParameters.push_back(objectToBeSent); 
+}
+HorizontalFlipImage::HorizontalFlipImage() : Skeleton("HorizontalFlipImage"), RegistrablePoolableCyclicCallableSkeleton("HorizontalFlipImage")
+{
+	// Aggiungo al serviceId e alla lista di input i parametri che mi aspetto di ricevere.
+	addParameter(new RawByteBuffer, IN);
+	addParameter(new RawByteBuffer, INOUT);
+}
+
