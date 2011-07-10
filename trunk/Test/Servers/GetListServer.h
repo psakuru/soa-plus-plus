17060f9ebe176ab29d60106ee08c91a05c7757b2
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-10 11:02:07 +0000

    fatto. devo ricontrollare gli include.

--- trunk/Test/Servers/GetListServer.h
+++ trunk/Test/Servers/GetListServer.h
@@ -19,23 +19,8 @@
  *
  */
 
-#include "../../ObjectInterfaces/SingletonObject/SingletonObject.h"
-#include "../../ObjectInterfaces/SerializableObject/SerializableObject.h"
-#include "../../SerializableObjects/Utilities/ByteArray/ByteArray.h"
 #include "../../ServiceOrientedArchitecture/Service/Skeleton/RegistrablePoolableCyclicCallableSkeleton/RegistrablePoolableCyclicCallableSkeleton.h"
-#include "../../ServiceOrientedArchitecture/Service/Skeleton/Utilities/PoolableCallableSkeletonWrappers/RegistrablePoolableCallableSkeletonWrapper/RegistrablePoolableCallableSkeletonWrapper.h"
-#include "../../ServiceOrientedArchitecture/Service/Skeleton/SkeletonThreadPool/RegistrableSkeletonThreadPool/RegistrableSkeletonThreadPool.h"
-#include "../../SerializableObjects/SerializationStrategies/RawByteBufferSerializationStrategy/RawByteBufferSerializationStrategy.h"
-#include "../../SerializableObjects/SerializationStrategies/IntegerSerializationStrategy/IntegerSerializationStrategy.h"
-#include "../../SerializableObjects/SerializationStrategies/StringSerializationStrategy/StringSerializationStrategy.h"
-#include "../../ServiceOrientedArchitecture/Publisher/Publisher.h"
-#include "../../ObjectInterfaces/RegistrableObject/RegistrableObject.h"
 #include "ImageRegisterSharedState.h"
-#include <string>
-#include <list>
-#include <iostream>
-#include <stdio.h>
-using namespace std;
 
 class GetList : public RegistrablePoolableCyclicCallableSkeleton
 {
@@ -43,25 +28,7 @@ private:
 	// Stato condiviso fra i thread del pool.
     ImageRegisterSharedState* sharedState;
 protected:
-    void doService()
-    {
-		// Acquisisco il lock sulla lista.
-        boost::shared_lock<boost::shared_mutex> readersLock(sharedState->sharedMutex);
-		// Inserisco la lista nei parametri di output.
-		string stringToReturn;
-		list<string>::iterator i = sharedState->imageList.begin();
-		for(; i != sharedState->imageList.end(); i++)
-        {
-			stringToReturn.append(*i).append(";");
-        }
-        outputParameters.push_back(new String(new string(stringToReturn), false));
-    }
+    void doService();
 public:
-    GetList() : Skeleton("GetList"), RegistrablePoolableCyclicCallableSkeleton("GetList")
-    {
-		// Stato condiviso fra i thread del pool. E' un'istanza singleton.
-        sharedState = SingletonObject<ImageRegisterSharedState>::getInstance();
-		// Aggiungo i parametri che mi aspetto di ricevere al serviceID e alla lista di input.
-        addParameter(new String, INOUT);
-    }
+    GetList();
 };
