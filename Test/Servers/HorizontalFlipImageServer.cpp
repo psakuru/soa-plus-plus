1ab864233e1ddab0bf870fe68b6847da9afc7ca5
davidesil.web@gmail.com <davidesil.web@gmail.com@d2f5aaf6-8d73-fad8-4cce-3e4437094ad0>
2011-07-12 17:17:02 +0000

    [No log message]

--- Test/Servers/HorizontalFlipImageServer.cpp
+++ Test/Servers/HorizontalFlipImageServer.cpp
@@ -30,7 +30,7 @@ void HorizontalFlipImageServer::doService()
 	ByteArray* bufferPointer = (ByteArray*)(r->getValue());
 	// Salvo l'immagine ricevuta.
 	string name = uniqueID;
-	storeImage(name,bufferPointer);
+	storeImage(name.c_str(),bufferPointer);
 	delete bufferPointer;
 	// Eseguo il flip.
 	CImg<unsigned char> image;
